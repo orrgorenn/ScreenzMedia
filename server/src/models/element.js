6a482a84951fec3cd9356008d9256c618dce4fe7
Orr Goren <orrgorenn@gmail.com>
2020-11-23 17:21:12 +0200

    Backend dev

---
+++ server/src/models/element.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const elementSchema = new mongoose.Schema({
+    bid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Building',
+        required: true
+    },
+    elementSource: {
+        type: String,
+        required: true
+    },
+    elementType: {
+        type: String,
+        enum: ['text', 'image', 'video']
+    },
+    transitionTime: {
+        type: Number,
+        min: 5,
+        required: true,
+        default: 15
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Element', elementSchema);
